73fd879d2c075ce5989571f8b1b8a5c1c545f7ad
mtmidhun25 <midhunmanojtp2525@gmail.com>
2026-06-17 15:36:58 +0530

    Changes made and js file created, updated

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
 function toggleMenu() {
-    document.getElementById("navLinks").classList.toggle("show");
+    document.getElementById("navLinks")
+            .classList.toggle("show");
 }
 
 function toggleTheme() {
     document.body.classList.toggle("dark");
 }
+
+document
+    .getElementById("contactForm")
+    .addEventListener("submit", function(event){
+
+    event.preventDefault();
+
+    let name =
+        document.getElementById("name").value;
+
+    let email =
+        document.getElementById("email").value;
+
+    alert(
+        "Form Submitted!\n\n" +
+        "Name: " + name +
+        "\nEmail: " + email
+    );
+
+});
